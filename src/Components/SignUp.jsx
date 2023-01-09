1cdbc1606078aa0acf679c2e4f2ae900de42a81e
AKSHAYPATILBHOSALE07 <bhosaleakshay566@gmail.com>
2023-01-09 15:10:31 +0530

    Added Login form and login success

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -26,8 +26,23 @@ export default function SignUp() {
         //setPostId('ID: ' + data.id + ' Username: ' + data.userName)
       );
 
-    // empty dependency array means this effect will only run once (like componentDidMount in classes)
-    //}, []);
+  };
+
+  const handleSubmitLogin = () => {
+    console.log(userName + ' ' + password);
+    console.log('111111111111111111');
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ userName: userName, password: password }),
+    };
+    fetch('http://localhost:8080/api/users/login', requestOptions)
+      .then((response) => response.json())
+      .then(
+        (data) => 
+        console.log(data)
+        //setPostId('ID: ' + data.id + ' Username: ' + data.userName)
+      );
   };
   return (
     <>
@@ -79,6 +94,49 @@ export default function SignUp() {
             </Grid>
           </Grid>
         </>
+        <Grid
+            container
+            spacing={5}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            style={{ minHeight: '100vh' }}
+            alignItems="center"
+          >
+            <Grid item>
+              <FormControl>
+                <h2>Login Form</h2>
+                <Grid item sx={{ p: 1 }}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Username"
+                    variant="outlined"
+                    onChange={(e) => setuserName(e.target.value)}
+                    value={userName}
+                  />
+                </Grid>
+                <Grid item sx={{ p: 1 }}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Password"
+                    variant="outlined"
+                    onChange={(e) => setpassword(e.target.value)}
+                    value={password}
+                  />
+                </Grid>
+                <Grid item sx={{ p: 1 }}>
+                  <Button
+                    variant="contained"
+                    value="submit"
+                    type="submit"
+                    onClick={handleSubmitLogin}
+                  >
+                    Log In
+                  </Button>
+                </Grid>
+              </FormControl>
+            </Grid>
+          </Grid>
       </div>
     </>
   );
