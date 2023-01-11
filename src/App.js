8d8a9c8bad6ec94f1bf2aaffe218afdc0bd24cc9
AKSHAYPATILBHOSALE07 <bhosaleakshay566@gmail.com>
2023-01-11 14:23:44 +0530

    When signup user goes to login page   3 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import SignUp from './Components/SignUp';
 import Main from './Components/Main';
 import Header from './Components/Header';
 import ResponsiveAppBar from './Components/ResponsiveAppBar';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Login from './Forms/Login';
 
 export default function App() {
   return (
     <>
-      <div>
-        <SignUp /> 
-        {/* <Header />
-        <Main /> */}
-        <ResponsiveAppBar />
-      </div>
+      <ResponsiveAppBar />
+      <SignUp />
     </>
   );
 }
